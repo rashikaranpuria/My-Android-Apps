8558e1002709945d53a1225a232cee4e4459108c
rashi007 <2013165@iiitdmj.ac.in>
2016-06-12 22:30:17 +0530

    setting part completed

--- MovieApp/app/src/main/java/com/example/abc/movieapp/MainActivity.java
+++ MovieApp/app/src/main/java/com/example/abc/movieapp/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -34,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Log.e("settings ","settings");
             startActivity(new Intent(this, SettingsActivity.class));
             return true;
         }
